338ba1c3305404eb4fb43491d4403f0622e60666
manishewag <160532465+manishewag@users.noreply.github.com>
2024-11-10 14:58:58 +0530

    last commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
-    origin: "https://golden-yeot-3af92e.netlify.app",
+    origin: "*",
     credentials: true,
 }));
 
