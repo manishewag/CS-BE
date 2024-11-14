1fb32935dbb229af478f2b6a6b93124ec5d1efdb
manishewag <160532465+manishewag@users.noreply.github.com>
2024-11-14 10:19:18 +0530

    last commit

--- index.js
+++ index.js
@@ -29,32 +29,30 @@ app.use(cors({
 }));
 
 
-
-
 mongoose.connect(process.env.MONGO_URL);
-
-// function getUserDataFromReq(req) {
-//   return new Promise((resolve, reject) => {
-//     jwt.verify(req.cookies.token, jwtSecret, {}, (err, userData) => {
-//       if (err) throw err;
-//       resolve(userData);
-//     });
-//   });
-// }
 
 function getUserDataFromReq(req) {
   return new Promise((resolve, reject) => {
-    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
-      if (err) {
-          err = {
-            name: 'JsonWebTokenError',
-            message: 'jwt malformed'
-          }
-      };
+    jwt.verify(req.cookies.token, jwtSecret, {}, (err, userData) => {
+      if (err) throw err;
       resolve(userData);
     });
   });
 }
+
+// function getUserDataFromReq(req) {
+//   return new Promise((resolve, reject) => {
+//     jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+//       if (err) {
+//           err = {
+//             name: 'JsonWebTokenError',
+//             message: 'jwt malformed'
+//           }
+//       };
+//       resolve(userData);
+//     });
+//   });
+// }
 
 
 app.get('/', (req, res) => {
