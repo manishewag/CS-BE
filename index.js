267dc841462599d76cf6d29f01b6018fba64588a
manishewag <160532465+manishewag@users.noreply.github.com>
2024-11-16 10:34:34 +0530

    last commit

--- index.js
+++ index.js
@@ -31,17 +31,16 @@ app.use(cors({
 
 mongoose.connect(process.env.MONGO_URL);
 
-// function getUserDataFromReq(req) {
-//   // const token = req.headers.authorization?.split(' ')[1] // Bearer token [1]
-//   return new Promise((resolve, reject) => {
-//     console.log(userData)
-//     resolve(userData);
-//     // jwt.verify(token, jwtSecret, {}, (err, userData) => {
-//     //   if (err) throw err;
-//     //   resolve(userData);
-//     // });
-//   });
-// }
+function getUserDataFromToken(req) {
+  // const token = req.headers.authorization?.split(' ')[1] // Bearer token [1]
+  return new Promise((resolve, reject) => {
+    // jwt.verify(token, jwtSecret, {}, (err, userData) => {
+      // if (err) throw err;
+      console.log(userData)
+      resolve(userData);
+    });
+  // });
+}
 
 // function getUserDataFromReq(req) {
 //   return new Promise((resolve, reject) => {
